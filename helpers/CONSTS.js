2939b2e8cca8b8ce936a5c5d04377b9a98359fd9
sarthak1991 <sarthakbatra@rocketmail.com>
2024-07-18 00:59:25 +0530

    Added const to private repo

---
+++ helpers/CONSTS.js
@@ -0,0 +1,13 @@
+const CONSTANTS = {
+  APPID: "0ffe0d748ec844129824e234b233cdc9",
+  CHANNEL: "srthk",
+  METHOD: "POST",
+  customerID: "aa5e3fc9c50d4a2895ab92fb2ae37c64",
+  customerSecret: "39b8218f2fe84dccb0f71d4eb95443ad",
+  rtcToken: "007eJxTYPiztstGb7qI2K71eefjPXRutWVOMnYXZjr744/INJc7Zt0KDAZpaakGKeYmFqnJFiYmhkaWFkYmqUbGJklGxsbJKcmWS4OmpjUEMjLc1PdmYmSAQBCflaG4qCQjm4EBADXmHwM=",
+  rtmpRegion: "ap",
+  RTMPUrl: "rtmp://a.rtmp.youtube.com/live2/u9p5-f6mk-1ytk-tbpm-e24w",
+  InjectUrl:"http://content.jwplatform.com/manifests/vM7nH0Kl.m3u8"
+};
+
+export default CONSTANTS;
